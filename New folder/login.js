46ffe6fe92523863f109cefa9973a8f634a5a2bb
Andrew102803 <106626454+Andrew102803@users.noreply.github.com>
2022-09-15 21:18:02 -0500

    all front end work done so far,
    includes html for all current pages as well as js files for login and regsitration

---
+++ New folder/login.js
@@ -0,0 +1,52 @@
+const email = $("#email");
+const password = $("#password");
+const form = $("form");
+
+const closeNotification = $("#close_error_banner");
+const banner = $("#error_banner");
+closeNotification.on("click", () => {
+  banner.addClass("is-hidden");
+});
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  banner.addClass("is-hidden");
+
+  const emailVal = email.val().trim();
+  const passwordVal = password.val().trim();
+
+  const errors = {};
+  if (!emailVal) {
+    errors.email = "Invalid email";
+  }
+  if (!passwordVal) {
+    errors.password = "Invalid password";
+  }
+
+  if (errors.email || errors.password) {
+    alert(`${Object.keys(errors)
+      .map((key) => {
+        return `${errors[key]}\r`;
+      })
+      .join("")}
+      `);
+  } else {
+    const res = await fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: emailVal, password: passwordVal }),
+    });
+    if (res.ok) {
+      window.location.href = "/";
+      return;
+    }
+
+    banner.removeClass("is-hidden");
+  }
+};
+
+form.on("submit", handleSubmit);
