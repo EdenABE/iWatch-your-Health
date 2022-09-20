ce79588411234bf1d0e6cff35cc65212d85b58b9
Andrew102803 <106626454+Andrew102803@users.noreply.github.com>
2022-09-20 18:39:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
+
+const sequelize = require("./config/connection");
+
+const { setNewToken, getUserIfCookieExists } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(getUserIfCookieExists);
 app.use(routes);
+app.use(setNewToken);
+
+// 404 page
+app.use(function (req, res) {
+  res.render("404", { title: "Oops! Page not found." });
+});
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(`Now listening at port ${PORT}! <=== Running!`)
+  );
 });
