1fd76916dc4d486b475ada3ae041d186c58d2074
EdenABE <eden.a.biltibo@vumc.org>
2022-09-23 00:05:52 -0400

    signup js data added

---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+  const age = document.querySelector('#age').value.trim();
+  const sex = document.querySelector('#sex').value.trim();
+
+  if (name && email && password && age && sex) {
+    const response = await fetch('/api/users/signup/', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password, age, sex }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
